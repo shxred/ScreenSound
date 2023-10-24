df9f7d02a9881047d2ef472c619e6affaaf32c1e
Eduardho Costa <99217833+shxred@users.noreply.github.com>
2023-10-24 20:25:47 -0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿Musica musica1 = new Musica();
+musica1.nome = "Sweet";
+musica1.artista = "Guns N Roses";
+
+Console.WriteLine($"Nome da música {musica1.nome} do artista {musica1.artista}");
+
+Musica musica2 = new Musica();
+musica2.nome = "One";
+musica2.artista = "Metallica";
+
+Console.WriteLine($"Nome da música {musica2.nome} do artista {musica2.artista}");
